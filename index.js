88b2ebfc1c6536788dbb6bc8e49c282540eac989
Candice Peters <candice.peters@flatironschool.com>
2021-04-11 17:33:26 -0500

    commit tree 97126da6d0aea791ada6c46177369e2d2e6ba776

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector('#main')
+main.remove() //remove main
+
+let body = document.querySelector('body') //grab the body
+let newHeader = document.createElement('h1') //create the h1
+newHeader.id = 'victory' //add the ID
+newHeader.innerHTML = "Candice is the champion" //add the text
+body.appendChild(newHeader) //append it to the body 
+
